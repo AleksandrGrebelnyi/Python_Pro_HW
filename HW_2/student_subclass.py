baf5d42940b2315482be04f1a33795febe0d175a
Aleksandr <aleksandr955@gmail.com>
2022-06-23 01:07:01 +0300

    add comment

--- HW_2/student_subclass.py
+++ HW_2/student_subclass.py
@@ -2,6 +2,10 @@ from person_superclass import Person
 
 
 class Student(Person):
+
+    """
+    Class Student is subclass of Person superclass
+    """
     total_students = 0
 
     def __init__(self, firstname, lastname, age: int):
