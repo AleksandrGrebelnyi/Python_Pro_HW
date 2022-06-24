7ee8f6aa8089ee6dfcd9d0b3a218ce765fd035a6
Aleksandr <aleksandr955@gmail.com>
2022-06-24 18:40:18 +0300

    some fixes

--- HW_3/task_2/exception.py
+++ HW_3/task_2/exception.py
@@ -1,9 +1,7 @@
 class GroupLimit(Exception):
 
     def __init__(self, limit, desc):
-        super().__init__()
-        self.limit = limit
         self.desc = desc
 
     def __str__(self):
-        return f"AmountError \n{self.limit} - {self.desc}"
+        return f"Group is full \n - - - {self.desc}"
