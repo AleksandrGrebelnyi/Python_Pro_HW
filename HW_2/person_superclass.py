40c18b2fb09b5f82d842d34e1834ee89f826f1da
Aleksandr <aleksandr955@gmail.com>
2022-06-24 18:31:03 +0300

    add

--- HW_2/person_superclass.py
+++ HW_2/person_superclass.py
@@ -5,8 +5,8 @@ class Person:
     """
 
     def __init__(self, firstname: str, lastname: str):
-        self.firstname = firstname
-        self.lastname = lastname
+        self.firstname = firstname.strip().title()
+        self.lastname = lastname.strip().title()
 
     def __str__(self):
         return f'{self.lastname}  {self.firstname}'
