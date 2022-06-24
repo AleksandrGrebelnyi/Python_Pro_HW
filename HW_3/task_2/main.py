7957597c6a29a29878aaf0374dfdde7d2d1f054f
Aleksandr <aleksandr955@gmail.com>
2022-06-24 18:42:17 +0300

    new idea

---
+++ HW_3/task_2/main.py
@@ -0,0 +1,45 @@
+# 2) Модифицируйте класс Группа (задание прошлой лекции) так, чтобы при
+# попытке добавления в группу более 10-ти студентов, было возбужденно
+# пользовательское исключение. Таким образом нужно создать еще и
+# пользовательское исключение для этой ситуации. И обработать его.
+from exception import GroupLimit
+from student import Student
+from group import Group
+from settings import LIMIT_OF_STUDENTS
+
+student_1 = Student('Ivan', 'Pupkin', 20)
+student_2 = Student('Valera', 'Reza', 19)
+student_3 = Student('Sasha', 'Zib', 20)
+student_4 = Student('Petr', 'Kira', 20)
+student_5 = Student('Sergey', 'Loza', 18)
+student_6 = Student('Pasha', 'Faster', 20)
+student_7 = Student('Valera', 'Guba', 20)
+student_8 = Student('Aleks', 'Lusha', 20)
+student_9 = Student('Ivan', 'Ivanov', 19)
+student_10 = Student('Oleh', 'Istratov', 19)
+student_11 = Student('Vaska', 'Petrov', 20)
+student_12 = Student('Pedro', 'Robz', 20)
+
+group_1 = Group('Python')
+try:
+    group_1.add_student(student_1)
+    group_1.add_student(student_2)
+    group_1.add_student(student_3)
+    group_1.add_student(student_4)
+    group_1.add_student(student_5)
+    group_1.add_student(student_6)
+    group_1.add_student(student_7)
+    group_1.add_student(student_8)
+    group_1.add_student(student_9)
+    group_1.add_student(student_10)
+    group_1.add_student(student_11)
+except GroupLimit as gl:
+    print(gl, 'Group is full, try later')
+
+# group_1.add_student(student_11)
+# group_1.add_student(student_12)
+
+print(group_1, '\n' + 'Total', Group.st)
+
+
+
