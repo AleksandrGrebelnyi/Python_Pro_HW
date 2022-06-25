c0504d5eb6fd0809a63ecebd417bf86e65da068a
Aleksandr <aleksandr955@gmail.com>
2022-06-25 18:05:26 +0300

    also made code better

--- HW_3/task_1/product.py
+++ HW_3/task_1/product.py
@@ -5,11 +5,12 @@ class Products:
     Created class Product
     """
     def __init__(self, name, price, descriptions, *args, **kwargs):
+        if price < 0:
+            raise NegativeNumber(price)
         self.name = name
         self.descriptions = descriptions
         self.price = price
-        if price < 0:
-            raise NegativeNumber(price, '\nInput positive number')
+
 
     def __str__(self):
         return f'{self.name} - {self.descriptions}, {self.price} UAH'
