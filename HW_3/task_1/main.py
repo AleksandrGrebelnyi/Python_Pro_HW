beb57329aa9b071e3fa64d877dfc4c1ca65c2df9
Aleksandr <aleksandr955@gmail.com>
2022-06-28 21:40:55 +0300

    it is main file

--- HW_3/task_1/main.py
+++ HW_3/task_1/main.py
@@ -8,17 +8,21 @@ from product import Products
 from buyer import Buyer
 from order import Order
 
-try:
-    product_1 = Products('apple', 23, 'sweet')
-    product_2 = Products('banana', 34, 'sweet')
-except NegativeNumber as ng:
-    print('Your number should not be negative')
+if __name__ == '__main__':
+
+    try:
+        product_1 = Products('apple', 23, 'sweet')
+        product_2 = Products('banana', 34, 'sweet')
+    except NegativeNumber as ng:
+        print('Your number should not be negative')
 
 
-client_1 = Buyer('Pupkin', 'Ivan', '93459734597')
+    client_1 = Buyer('Pupkin', 'Ivan', '93459734597')
 
-order_1 = Order(client_1)
-order_1.add_product(product_1, 2)
-order_1.add_product(product_2, 1)
-order_1.add_product(product_2, 2)
-print(order_1)
+    order_1 = Order(client_1)
+    order_1.add_product(product_1, 2)
+    order_1.add_product(product_2, 1)
+    order_1.add_product(product_2, 2)
+    print(order_1)
+
+    print(__name__)
