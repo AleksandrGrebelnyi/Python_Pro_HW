63a6e04194a35635a17509537a348fcb45e5d8b7
Aleksandr <aleksandr955@gmail.com>
2022-07-01 10:44:47 +0300

    fix code

--- HW_4/correct_fraction.py
+++ HW_4/correct_fraction.py
@@ -1,9 +1,6 @@
 # Создайте класс «Правильная дробь» и реализуйте методы сравнения,
 # сложения, вычитания и произведения для экземпляров этого класса.
 
-from fractions import Fraction
-
-
 class CorrectFraction:
 
     def __init__(self, numerator: int, denominator: int):
@@ -13,40 +10,45 @@ class CorrectFraction:
         self.denominator = denominator
 
     def __add__(self, other):
-        return f'Summa: {Fraction(self.numerator, self.denominator) + Fraction(other.numerator, other.denominator)}'
+        return CorrectFraction(self.numerator * other.denominator + other.numerator * self.denominator,
+                               self.denominator * other.denominator)
 
     def __sub__(self, other):
-        return f'Subtraction: {Fraction(self.numerator, self.denominator) - Fraction(other.numerator, other.denominator)}'
+        return CorrectFraction(self.numerator * other.denominator - other.numerator * self.denominator,
+                               self.denominator * other.denominator)
 
     def __mul__(self, other):
-        return f'Multipy: {Fraction(self.numerator, self.denominator) * Fraction(other.numerator, other.denominator)}'
+        return CorrectFraction(self.numerator * other.numerator, other.denominator * self.denominator)
 
     def __eq__(self, other):
-        return f'Equal?: {Fraction(self.numerator, self.denominator) == Fraction(other.numerator, other.denominator)}'
+        return self.numerator / self.denominator == other.numerator / other.denominator
 
     def __ne__(self, other):
-        return f'Equal?: {Fraction(self.numerator, self.denominator) != Fraction(other.numerator, other.denominator)}'
+        return self.numerator / self.denominator != other.numerator / other.denominator
 
     def __gt__(self, other):
-        return f'Equal?: {Fraction(self.numerator, self.denominator) > Fraction(other.numerator, other.denominator)}'
+        return self.numerator / self.denominator > other.numerator / other.denominator
 
     def __lt__(self, other):
-        return f'Equal?: {Fraction(self.numerator, self.denominator) < Fraction(other.numerator, other.denominator)}'
+        return self.numerator / self.denominator < other.numerator / other.denominator
 
     def __ge__(self, other):
-        return f'Equal?: {Fraction(self.numerator, self.denominator) >= Fraction(other.numerator, other.denominator)}'
+        return self.numerator / self.denominator >= other.numerator / other.denominator
 
     def __le__(self, other):
-        return f'Equal?: {Fraction(self.numerator, self.denominator) <= Fraction(other.numerator, other.denominator)}'
+        return self.numerator / self.denominator <= other.numerator / other.denominator
 
     def __str__(self):
-        return f'{self.numerator}/{self.denominator}'
+        if self.numerator < self.denominator:
+            return f'Correct fraction {self.numerator} / {self.denominator}\n'
+        else:
+            return f'Incorrect fraction {self.numerator} / {self.denominator}'
 
 fr_1 = CorrectFraction(1, 7)
 fr_2 = CorrectFraction(2, 5)
-print(fr_1 + fr_2)
-print(fr_1 - fr_2)
-print(fr_1 * fr_2)
-print(fr_1 == fr_2)
+print('Plus = ', fr_1 + fr_2)
+print('Minus = ', fr_1 - fr_2)
+print('Multipy = ', fr_1 * fr_2)
+print('Equal? ', fr_1 == fr_2)
 print(fr_1, fr_2)
-print(fr_1 != fr_2)
+print('Not equal? ', fr_1 != fr_2)
