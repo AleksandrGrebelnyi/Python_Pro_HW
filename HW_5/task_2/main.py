39f19af2c56a7cf589c94ef4f5d6ccc2f84ba258
Aleksandr <aleksandr955@gmail.com>
2022-07-06 11:26:38 +0300

    improved

--- HW_5/task_2/main.py
+++ HW_5/task_2/main.py
@@ -10,6 +10,8 @@ from order import Order
 
 product_1 = Products('apple', 20, 'sweet')
 product_2 = Products('banana', 30, 'very good')
+product_3 = Products('paper', 47, 'very good')
+product_4 = Products('avocado', 70, 'good')
 
 client_1 = Buyer('Pupkin', 'Ivan', '93459734597')
 
@@ -17,4 +19,20 @@ order_1 = Order(client_1)
 order_1.add_product(product_1, 2)
 order_1.add_product(product_2, 1)
 order_1.add_product(product_2, 2)
+order_1.add_product(product_3, 1)
+order_1.add_product(product_4, 2)
 print(order_1)
+
+# итерация работает
+for prod in order_1:
+    print(prod.name)
+print('*' * 45)
+
+# по идексу работает
+print(order_1[1])
+print('*' * 45)
+
+# по срезу работает
+prod_slice = order_1[0:]
+for prod in prod_slice:
+    print(prod)
