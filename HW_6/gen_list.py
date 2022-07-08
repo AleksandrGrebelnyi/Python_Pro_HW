081e27a90b0853ea49e8f5967aa99474bfaff0a7
Aleksandr <aleksandr955@gmail.com>
2022-07-08 09:22:14 +0300

    gen express

--- HW_6/gen_list.py
+++ HW_6/gen_list.py
@@ -2,5 +2,7 @@
 # быть заполнен кубами чисел от 2 и до указанной вами величины.
 
 n = int(input('Input number: '))
-cube_list = [x ** 3 for x in range(2, n)]
-print(cube_list)
+cube_list = (x ** 3 for x in range(2, n))
+
+for cube in cube_list:
+    print(cube, end=' ')
